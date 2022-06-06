f93b1ec4b50540bc2d6f8e655020fb9e5fc7cb1f
TheJohnzer <axeljonathan208@gmail.com>
2022-06-06 15:49:18 -0700

    stracth

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,44 +4,43 @@
  *
  * Return: Always 0 (Success/correct)
  */
- int main(void)
- 
-  }
- int left;
- int right;
- int center;
-
-
-  for (left = 48; left <= 57; left++)
-{
-  for (center = left + 1; center <= 57; center++)
+int main(void)
 
 {
-  for (right = center + 1 ; right <= 57; right++)
+	int left;
+	int right;
+	int center;
 
 
-       {
-	  putchar(left);
-	  putchar(center);
-	  putchar(right);
 
-	  if  ((left == 55) && (center == left + 1) && (right == center + 1))
+	for (left = 48; left <= 57; left++)
+	{
+		for (center = left + 1; center <= 57; center++)
+		{
+			for (right = center + 1 ; right <= 57; right++)
 
-         {
-	      break;
-	      {
 
-	      putchar(',');
+			{
+				putchar(left);
+				putchar (center);
+				putchar (right);
 
-	      putchar (' ');
+				if  ((left == 55) && (center == left + 1) && (right == center + 1))
+				{
+					break;
+				}
 
-         }
+				putchar(',');
+				putchar (' ');
 
-         }  
+			}
 
-         }
+		}
 
-         putchar('\n');
+	}
 
-	 return (0);
-	 }
+	putchar('\n');
+
+	return (0);
+
+}
