d82c1cc11bf4370ebd6cf6bca524e1a7c7f65e77
TheJohnzer <axeljonathan208@gmail.com>
2022-06-14 09:19:36 -0700

    glory

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,14 @@
-#include "holberton.h"
+#include "holbertonh"
 
 /**
- * reset_to_98 - changes the value of an int it points to
- * @n: pointer to an int
- * Return: (0)
+ *reset_to_98 - updates a integer value to 98
+ *@n: A pointer to an int that will be updated
+ *
+ *Return: void that means is correct
  */
 
 void reset_to_98(int *n)
+
 {
-		*n = 98;
+	*n = 98;
 }
--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,22 @@
 #include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-/**
- */
+
 int main(void)
 {
-	int num;
+	int rand_passwd;
+	int count;
+	int total;
 
 	srand(time(0));
-	num = rand();
-	printf("%i\n", num);
+	for (count = 0, total = 2772; total > 122; count++)
+	{
+	rand_passwd = (rand() % 125) + 1;
+	printf("%c", rand_passwd);
+	total -= rand_passwd;
+	}
+	printf("%c", total);
+
 	return (0);
 }
