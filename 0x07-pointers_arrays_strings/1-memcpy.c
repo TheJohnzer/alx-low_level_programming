0f4391386118933a8171b86d4b837dd1abddd4a0
TheJohnzer <axeljonathan208@gmail.com>
2022-06-20 14:38:37 -0700

    yeah

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,25 +1,25 @@
 #include "holberton.h"
-
 /**
- * _memcpy - copy memory area
- * @dest: Destination for copying
- * @src: Source to copy from
- * @n: The number of bytes to copy
- * Return: A  pointer to dest.
+ *_memcpy -   copy number bytes form adress "from" to adress "to"
+ *@dest: content is to be copied
+ *@src: Source of data to be copied
+ *@n: bytes of the memory
+ *Return: dest
  */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
+
 {
+	unsigned int i;
 
-		int i = 0;
-		int j = 0;
+	i = 0;
+	while (i < n)  /*Delacring WHILE*/
+	{
+		*(dest + i) = *(src + i); /*add 1 position dest and src*/
+		i++;
 
-		while (n > 0)
-		{
+	} /*END WHILE*/
 
-			dest[i] = src[j];								i++;										j++;
-			n--;
 
-		}
-		return (dest);
+	return (dest);
 }
